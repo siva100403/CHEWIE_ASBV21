5d34879c4a96d858e818ded402f43ed2319c7c7c
jawaharmankomb <jawahar@mankomb.com>
2026-03-17 16:10:28 +0530

    WIP

--- source/alarm.h
+++ source/alarm.h
@@ -1,21 +1,21 @@
 /*
- * alert.h
+ * alarm.h
  *
  *  Created on: 29-Dec-2024
  *      Author: Jawahar Arumugam
  */
 
-#ifndef ALERT_H_
-#define ALERT_H_
+#ifndef ALARM_H_
+#define ALARM_H_
 
 
 
 /************************************************************************
- * 							Chewie Alert Definitions 					*
+ * 							Chewie Alarm Definitions 					*
  *
  ************************************************************************/
 
-#define ALERT_WASTE_ADDITION_TOO_LONG		0
+
 #define ALERT_TRANSFER_PENDING				1			//This is due to STORAGE_TRAY_FULL or STORAGE_TRAY_OPEN
 #define ALERT_STORAGE_TRAY_FULL				2
 #define ALERT_STORAGE_TRAY_OPEN				3
@@ -28,4 +28,4 @@
 void generateAlert(uint16_t alert);
 
 
-#endif /* ALERT_H_ */
+#endif /* ALARM_H_ */
